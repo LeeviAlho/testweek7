b8521b868101542f6826f6517e866b9d57e5689c
Leevi Alho <54808340+LeeviAlho@users.noreply.github.com>
2021-03-08 23:24:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/testweek7/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.testweek7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    EditText edit;
+    String input;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
+        edit = (EditText) findViewById(R.id.editText);
+
+
+    }
+
+    public void  testMethod(View v){
+        System.out.println("Hello World!");
+        input = edit.getText().toString();
+        //text.setText("Hello World!");
+        text.setText(input);
+    }
+}
